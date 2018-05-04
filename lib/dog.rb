b0dc6f5f88608057ee37c3511be99a16edbaaa05
Maria  Morris <mariasemailbox@gmail.com>
2018-05-04 03:05:32 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,17 @@
 class Dog
-  def name=(dog_name)
-    this_dogs_name = dog_name
+  def name=(name)
+    @name = name
   end
 
   def name
-    this_dogs_name
+    @name
   end
 
-  def breed=(dog_breed)
-    this_dogs_breed = dog_breed
+  def breed=(breed)
+    @breed = breed
   end
 
   def breed
-    this_dogs_breed
+    @breed
   end
 end
