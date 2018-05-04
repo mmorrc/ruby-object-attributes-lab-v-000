ed6fbcf441b33503af2031f4d480aa3d44b3985a
Maria  Morris <mariasemailbox@gmail.com>
2018-05-04 03:01:45 +0000

    automatically backed up by learn

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name=(person_name)
+    this_persons_name = person_name
+  end
+
+  def name
+    this_persons_name
+  end
+
+  def job=(person_job)
+    this_persons_job = person_job
+  end
+
+  def job
+    this_persons_job
+  end
+end
